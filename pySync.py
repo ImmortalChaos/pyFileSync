c634b7390742f2ab723c9f2e7bfc4958a4f9ed52
MinHo, Park <pminho0503@hanmail.net>
2019-02-16 08:25:26 +0900

    #3 create FileChecker Class (#5)
    * create FileChecker Class . 
       - Read source and target folders

--- pySync.py
+++ pySync.py
@@ -1,7 +1,80 @@
-import os
+import os, time
+import hashlib
 import argparse
+import pickle
 
 exclude_folder = ['.git', '.svn']
+
+HAS_NOT_INDEX = -1
+FOLDER_NOT_SET = 0
+FOLDER_READ_ONLY = 1
+FOLDER_SYNC = 2
+
+FILE_STATUS_NORMAL= 0
+FILE_STATUS_DELETED = 1
+
+def md5sum_file(filename, blocksize=65536):
+	hash = hashlib.md5()
+	with open(filename, "rb") as f:
+		for block in iter(lambda: f.read(blocksize), b""):
+			hash.update(block)
+	return hash.hexdigest()
+
+def md5sum_str(text):
+	hash = hashlib.md5()
+	m.update(text)
+	return m.hexdigest()
+
+class FileChecker:
+	def __init__(self, folderSlotCount):
+		self.savedFileName = ""
+		self.currentFilePathIndex = HAS_NOT_INDEX
+		self.currentFilePath = ""
+		self.folderList = list(map(lambda n: {"Index":n, "Mode":FOLDER_NOT_SET, "Path":""}, range(folderSlotCount)))
+		self.fileList = list(map(lambda n: {}, range(folderSlotCount)))
+
+	def __del__(self) :
+		pass
+
+	def createFolderInfo(self, path, mode) :
+		for i in range(len(self.folderList)) :
+			if self.folderList[i]["Path"]=="" :	
+				self.folderList[i]["Mode"] = mode
+				self.folderList[i]["Path"] = path
+				return i
+		return HAS_NOT_INDEX
+
+	def getFolderIndex(self, path) :
+		for i in range(len(self.folderList)) :
+			if self.folderList[i]["Path"]==path :
+				return i
+		return HAS_NOT_INDEX
+
+	def appendFolder(self, path, folderType):
+		findIdx = self.getFolderIndex(path)
+		if findIdx!=HAS_NOT_INDEX :
+			self.currentFilePathIndex = findIdx
+			self.folderList[findIdx]["Mode"] = folderType
+		else :
+			self.currentFilePathIndex = self.createFolderInfo(path, folderType)
+		self.currentFilePath = path
+
+	def appendFile(self, path):
+		fileHash = md5sum_file(path)
+		modifiedTime = os.path.getmtime(path)
+		createTime = os.path.getctime(path)
+		accessTime = os.path.getatime(path)
+
+		newPath = path[len(self.currentFilePath):]
+		self.fileList[self.currentFilePathIndex][newPath] = {"Hash":fileHash, "CTime":createTime, "MTime":modifiedTime, "ATime":accessTime, "Status":FILE_STATUS_NORMAL}
+
+	def saveStatus(self):
+		with open('data.pickle', 'wb') as f:
+			pickle.dump(self.fileList, f, pickle.HIGHEST_PROTOCOL)
+
+	def loadStatus(self):
+		with open('data.pickle', 'rb') as f:
+			self.fileList = pickle.load(f)
 
 def isExcludeDir(dirName) :
 	global exclude_folder
@@ -9,18 +82,19 @@ def isExcludeDir(dirName) :
 		return True
 	return False
 
-def search(dirname):
-    try:
-        filenames = os.listdir(dirname)
-        for filename in filenames:
-            full_filename = os.path.join(dirname, filename)
-            if os.path.isdir(full_filename) and not isExcludeDir(filename):
-                search(full_filename)
-            else:
-                ext = os.path.splitext(full_filename)[-1]
-                print(full_filename)
-    except PermissionError:
-        pass
+def search(dirname, fchk):
+	try:
+	    filenames = os.listdir(dirname)
+	    for filename in filenames:
+	        full_filename = os.path.join(dirname, filename)
+	        if os.path.isdir(full_filename) :
+	        	if not isExcludeDir(filename):
+	        		search(full_filename, fchk)
+	        else:
+	            ext = os.path.splitext(full_filename)[-1]
+	            fchk.appendFile(full_filename)
+	except :
+		print "Error"
 
 def main():
 	parser = argparse.ArgumentParser(description='This program synchronizes two different folders.')
@@ -36,7 +110,13 @@ def main():
 	sourceFolder = args.source
 	targetFolder = args.target
 
-	search(sourceFolder)
+	folderSlotCount = 2
+	fchk = FileChecker(folderSlotCount)
+	fchk.appendFolder(sourceFolder, FOLDER_READ_ONLY)
+	search(sourceFolder, fchk)
+
+	fchk.appendFolder(targetFolder, FOLDER_SYNC)
+	search(targetFolder, fchk)
 	print "Source Folder :", sourceFolder
 	print "Target Folder :", targetFolder
 
